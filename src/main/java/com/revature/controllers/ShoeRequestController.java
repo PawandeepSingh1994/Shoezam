4ba244ffaa44e6827258ee3b4fa94015bd8d9fc5
Pawandeep Singh <pawandeep.singh.kular@gmail.com>
2018-11-29 17:31:29 -0500

    after the CORS annotation

--- src/main/java/com/revature/controllers/ShoeRequestController.java
+++ src/main/java/com/revature/controllers/ShoeRequestController.java
@@ -3,6 +3,7 @@ package com.revature.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import com.revature.beans.ShoeRequest;
 import com.revature.services.ShoeRequestService;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/swaps")
 public class ShoeRequestController {
 	
